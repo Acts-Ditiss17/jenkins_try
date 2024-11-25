1413df2c4ecfa728bf9f87673206d511f3f2fca8
vijay <dkey4150@gmail.com>
2024-11-25 23:16:45 +0530

    hello everyone

---
+++ hello.java
@@ -0,0 +1,7 @@
+// This is a simple Java program to print "Hello, World!" to the console.
+
+public class HelloWorld { // Class declaration
+    public static void main(String[] args) { // Main method: the entry point of the program
+        System.out.println("Hello, World!"); // Prints "Hello, World!" to the console
+    }
+}
